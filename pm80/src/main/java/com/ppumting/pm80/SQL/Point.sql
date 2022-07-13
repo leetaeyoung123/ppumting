616aaa6676307e52aa720425f2e0964b573826bf
psg3180 <103983349+parkSangGyu98@users.noreply.github.com>
2022-07-13 11:37:17 +0900

    point sql success
    point sql success

--- pm80/src/main/java/com/ppumting/pm80/SQL/Point.sql
+++ pm80/src/main/java/com/ppumting/pm80/SQL/Point.sql
@@ -1,7 +1,9 @@
 CREATE TABLE Point(
 	aid			BIGINT		PRIMARY KEY 	AUTO_INCREMENT,
 	point		BIGINT		NOT NULL,
-	accountNum	VARCHAR(11)	NOT NULL
+	accountNum	VARCHAR(11)	NOT NULL,
+	userNumber	BIGINT		NOT NULL,
+	CONSTRAINT Point_userNumber_FK FOREIGN KEY (userNumber) REFERENCES Users(userNumber)
 ) AUTO_INCREMENT = 1001;
 
 -- 	Point테이블에 추가해줄거 
@@ -10,5 +12,27 @@ CREATE TABLE Point(
 
 
 SELECT * FROM Point;
+SELECT * FROM Users;
 
-INSERT INTO Point (point, accountNum) VALUES (50000, '123-45-6789')
+DROP TABLE Point;
+
+INSERT INTO Point (point, accountNum, userNumber) VALUES (70000, '789-45-1234', 2)
+
+-- inner join
+SELECT * FROM Account, Customer WHERE Account.customerId = Customer.cid
+AND Customer.ssn = '981212-1111111';
+
+INSERT INTO Users (name, ssn, phone, userId, pw, addr, aid)
+VALUES('박상규', '980618', '010-3490-3180', 'psg', '1234', 'daegu', '2');
+
+SELECT * FROM Point p INNER JOIN Users u ON p.userNumber = u.userNumber;
+SELECT * FROM Point p INNER JOIN Users u ON p.userNumber = u.userNumber WHERE u.name = '이태영';
+
+DELETE FROM Point WHERE name = '이태영';
+
+
+
+
+
+
+
--- pm80/src/main/java/com/ppumting/pm80/SQL/User.sql
+++ pm80/src/main/java/com/ppumting/pm80/SQL/User.sql
@@ -8,12 +8,16 @@ CREATE TABLE Users (
 	addr VARCHAR(25) NOT NULL,
 	aid  BIGINT NOT NULL,
 	ring BIGINT NOT NULL DEFAULT 0
+	CONSTRAINT User_aid_FK FOREIGN KEY (aid) REFERENCES Point(aid)
 )AUTO_INCREMENT = 1;
+
 SELECT * FROM Users;
+
 DROP TABLE User;
 
 INSERT INTO Users (name, ssn, phone, userId, pw, addr, aid)
 VALUES('이태영', '970312', '010-2881-2498', 'lty', '1234', 'daegu', '1');
+
 
 --FK
 --CONSTRAINT User_aid_FK FOREIGN KEY (aid) REFERENCES Point(aid),
