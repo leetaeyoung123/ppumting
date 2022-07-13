87653c8735816ed8c5a992798c96b09273631127
sujeong <0512bky@gmail.com>
2022-07-13 11:43:33 +0900

    Update Trainer.sql

--- pm80/src/main/java/com/ppumting/pm80/SQL/Trainer.sql
+++ pm80/src/main/java/com/ppumting/pm80/SQL/Trainer.sql
@@ -1,15 +1,17 @@
 CREATE TABLE Trainers (
-	TrainerNumber 	BIGINT PRIMARY KEY AUTO_INCERMENT,
+	trainerNumber 	BIGINT PRIMARY KEY AUTO_INCREMENT,
 	name			VARCHAR(20) NOT NULL,
 	ssn 			VARCHAR(7) 	NOT NULL,
 	phone 			VARCHAR(13) NOT NULL,
 	trainerId 		VARCHAR(16) NOT NULL,
 	pw 				VARCHAR(20) NOT NULL,
-	addr 			VARCHAR(25) NOT NULL
+	addr 			VARCHAR(25) NOT NULL,
+	aid  			BIGINT 		NOT NULL,
+	ring 			BIGINT 		NOT NULL DEFAULT 0
 )AUTO_INCREMENT = 1;
 
 SELECT * FROM Trainers;
 DROP TABLE Trainers;
 
-INSERT INTO Trainers (name, ssn, phone, userId, pw, addr, aid)
+INSERT INTO Trainers (name, ssn, phone, trainerId, pw, addr, aid)
 VALUES('백기열', '950512', '010-8538-8436', 'bky', '1234', 'daegu', '1');
