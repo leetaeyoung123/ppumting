a228397376eade8ee1381d3277523559003522a2
workhan0918 <workhan0918@gmail.com>
2022-07-13 10:33:25 +0900

    note sql 추가
    note sql 추가

--- pm80/src/main/java/com/ppumting/pm80/SQL/Note.sql
+++ pm80/src/main/java/com/ppumting/pm80/SQL/Note.sql
@@ -1,1 +1,13 @@
 SHOW TABLES;
+
+CREATE TABLE `senduser` (
+  `msg` VARCHAR(200) NOT NULL,
+  `sendDate` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`msg`));
+
+CREATE TABLE `reciveuser` (
+  `ring` BIGINT NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`ring`));  
+  
+SELECT * FROM senduser;
+SELECT * FROM reciveuser;
