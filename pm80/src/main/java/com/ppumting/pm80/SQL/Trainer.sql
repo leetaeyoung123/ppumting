f8824c483b27d77783da762e417118641b4cc51a
sujeong <0512bky@gmail.com>
2022-07-13 11:34:05 +0900

    Update Trainer.sql

---
+++ pm80/src/main/java/com/ppumting/pm80/SQL/Trainer.sql
@@ -0,0 +1,13 @@
+CREATE TABLE Trainers (
+	TrainerNumber 	BIGINT PRIMARY KEY AUTO_INCERMENT,
+	name			VARCHAR(20) NOT NULL,
+	ssn 			VARCHAR(7) 	NOT NULL,
+	phone 			VARCHAR(13) NOT NULL,
+	trainerId 		VARCHAR(16) NOT NULL,
+	pw 				VARCHAR(20) NOT NULL,
+	addr 			VARCHAR(25) NOT NULL
+
+)AUTO_INCREMENT = 1;
+
+SELECT * FROM Trainers;
+DROP TABLE Trainers;
