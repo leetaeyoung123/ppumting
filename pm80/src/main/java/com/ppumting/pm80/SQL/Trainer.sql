41d7bd84b9916a907944202a0d6d329afc34822a
sujeong <0512bky@gmail.com>
2022-07-13 12:20:20 +0900

    0713
    0713

--- pm80/src/main/java/com/ppumting/pm80/SQL/Trainer.sql
+++ pm80/src/main/java/com/ppumting/pm80/SQL/Trainer.sql
@@ -9,6 +9,9 @@ CREATE TABLE Trainers (
 	aid  			BIGINT 		NOT NULL,
 	ring 			BIGINT 		NOT NULL DEFAULT 0
 )AUTO_INCREMENT = 1;
+	-- CONSTRAINT Trainers_aid_FK FOREIGN KEY (aid) REFERENCES Point(aid)
+
+ALTER TABLE Trainers ADD CONSTRAINT Trainers_aid_FK FOREIGN KEY (aid) REFERENCES Point(aid);
 
 SELECT * FROM Trainers;
 DROP TABLE Trainers;
