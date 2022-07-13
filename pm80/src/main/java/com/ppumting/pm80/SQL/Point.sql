fab6b04976cbc80c1dcf219d0d0d0dde11d870b6
workhan0918 <workhan0918@gmail.com>
2022-07-13 12:16:11 +0900

    노트 sql
    노트 sql

--- pm80/src/main/java/com/ppumting/pm80/SQL/Point.sql
+++ pm80/src/main/java/com/ppumting/pm80/SQL/Point.sql
@@ -1,7 +1,9 @@
 CREATE TABLE Point(
 	aid			BIGINT		PRIMARY KEY 	AUTO_INCREMENT,
+	userNumber	BIGINT		NOT NULL,
 	point		BIGINT		NOT NULL,
-	accountNum	VARCHAR(11)	NOT NULL
+	accountNum	VARCHAR(11)	NOT NULL,
+	CONSTRAINT Point_userNumber_FK FOREIGN KEY (userNumber) REFERENCES Users(userNumber)
 ) AUTO_INCREMENT = 1001;
 
 -- 	Point테이블에 추가해줄거 
--- pm80/src/main/java/com/ppumting/pm80/SQL/REPLAY.sql
+++ pm80/src/main/java/com/ppumting/pm80/SQL/REPLAY.sql
@@ -2,6 +2,8 @@ CREATE TABLE REPLAY(
 	replay_no			BIGINT			PRIMARY KEY AUTO_INCREMENT,
 	replay_content		VARCHAR(4000)	NOT NULL,
 	qna_no				BIGINT			NOT NULL,
-	user_number			BIGINT			NOT NULL
-	-- CONSTRAINT QNA_user_number_FK FOREIGN KEY (user_number) REFERENCES Users(userNumber)
-)
+	user_number			BIGINT			NOT NULL,
+	CONSTRAINT REPLAY_user_number_FK FOREIGN KEY (user_number) REFERENCES Users(userNumber)
+);
+
+SELECT * FROM REPLAY;
