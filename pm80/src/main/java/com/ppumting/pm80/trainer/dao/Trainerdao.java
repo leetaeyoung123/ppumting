13be6be2750d6dfe89ab577c584a2bd1dd76cc54
sujeong <0512bky@gmail.com>
2022-07-14 09:29:39 +0900

    Update Trainerdao.java

--- pm80/src/main/java/com/ppumting/pm80/trainer/dao/Trainerdao.java
+++ pm80/src/main/java/com/ppumting/pm80/trainer/dao/Trainerdao.java
@@ -16,18 +16,18 @@ public class TrainerDao {
 	public static TrainerDao getInstance() {
 		return instance;
 	}
-	
+
 	static NamingService namingService = NamingService.getInstance();
 	static DataSource datasource = (DataSource)namingService.getAttribute("dataSource");
-   
+
 	public TrainerDao() {
-		
+
 	}
-	
+
      public void addTrainer(Trainer trainer) {
          String sql = "INSERT INTO Trainer(trainerId, name, ssn, phone, passwd, addr)"
                   + "VALUES(?, ?, ?, ?, ?, ?)";
-         
+
          try {
             Connection con = null;
             PreparedStatement  pstmt = null;
@@ -54,9 +54,9 @@ public class TrainerDao {
 
      public List<Trainer> findAllTrainers() {
          String sql = "SELECT * FROM Trainer";
-         
+
          List<Trainer> trainerList = new ArrayList<>();
-         
+
          try {
             Connection con = null;
             PreparedStatement  pstmt = null;
@@ -86,7 +86,7 @@ public class TrainerDao {
 
 	public boolean isValidTrainers(String trainerId, String passwd) {
 	    String sql = "SELECT trainerId, passwd FROM Trainer WHERE trainerId = ?";
-        
+
         try {
            Connection con = null;
            PreparedStatement  pstmt = null;
@@ -96,7 +96,7 @@ public class TrainerDao {
               pstmt = con.prepareStatement(sql);
               pstmt.setString(1, trainerId);
               rs = pstmt.executeQuery();
-              
+
               Trainer trainerInfo = new Trainer();
               while(rs.next()) {
            	   trainerInfo.setTrainerId(rs.getString("trainerId"));
@@ -116,5 +116,5 @@ public class TrainerDao {
 		return false;
 	}
 
-     
+
    }
