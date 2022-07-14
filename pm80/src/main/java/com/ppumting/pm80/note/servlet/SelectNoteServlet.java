929a5319c9b4a361f31e1b519c9a77d5f7f9913a
workhan0918 <workhan0918@gmail.com>
2022-07-14 12:50:53 +0900

    note update
    note update

---
+++ pm80/src/main/java/com/ppumting/pm80/note/servlet/SelectNoteServlet.java
@@ -0,0 +1,18 @@
+package com.ppumting.pm80.note.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/Note/selectMsg")
+public class SelectNoteServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+
+}
