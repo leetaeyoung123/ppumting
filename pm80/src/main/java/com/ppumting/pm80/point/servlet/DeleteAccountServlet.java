33949946152aa022371ac627d96f645fb76a31f9
psg3180 <103983349+parkSangGyu98@users.noreply.github.com>
2022-07-18 18:24:02 +0900

    ㅇㄴ
    ㄴㅇㅁ

--- pm80/src/main/java/com/ppumting/pm80/point/servlet/DeleteAccountServlet.java
+++ pm80/src/main/java/com/ppumting/pm80/point/servlet/DeleteAccountServlet.java
@@ -2,13 +2,12 @@ package com.ppumting.pm80.point.servlet;
 
 import java.io.IOException;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class DeleteAccountServlet
- */
+@WebServlet("/point/deleteAccount")
 public class DeleteAccountServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
